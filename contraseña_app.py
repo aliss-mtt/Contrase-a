15c288f65d68a7589006fdee89aba9c94817f5b3
aliss-mtt <alisonn.bri.om@gmail.com>
2026-01-04 17:05:05 -0500

    new bot

--- contraseña_app.py
+++ contraseña_app.py
@@ -1,10 +1,26 @@
-import random
+import discord
+from discord.ext import commands
 
-caracteres = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
-pregunta = int(input("introduzca la longitud de la contrasena"))
-contrasena = ""
+intents = discord.Intents.default()
+intents.message_content = True
 
-for i in range(pregunta):
-    contrasena =contrasena+ random.choice(caracteres)
+bot = commands.Bot(command_prefix='$', intents=intents)
 
-print ("tu contrasena es:", contrasena)
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hola, soy un bot {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+    
+@bot.command()
+async def add(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(left + right)
+
+bot.run(")
